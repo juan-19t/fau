d2d612936cd62d95f1e9ba0795f83a13a8194640
Juan Paulo Trentino <69488521+juan-19t@users.noreply.github.com>
2024-12-02 15:48:08 -0300

    Delete fau directory

--- fau/fauapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FauappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fauapp'
--- fau/fauapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def login_custom(request):
-    return render (request,'admin/login.html')
